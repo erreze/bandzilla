e1a54a15d2c5f5ad8a42756513237a35f95e937c
raulooo <erreauele@gmail.com>
2013-08-29 08:26:52 -0500

    adding site and fixing ajax calls

--- js/main2.js
+++ js/main2.js
@@ -1,36 +1,99 @@
+// var myapp = angular.module('myapp', ['firebase']);
+
+function addtrack(ix, sound_url){
+  var jplayerId = "jpId_"+ ix;
+
+      $("#trackContainer").append( "<div id='"+jplayerId+"'></div>");
+      $("#"+jplayerId).jPlayer( {
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            oga: sound_url
+          })
+        },
+        supplied: "oga",
+      });
+
+      var chartId="jpId_chart_"+ ix;
+
+      $("#trackContainer").append( "<div id='"+chartId+"'></div>");
+      console.log($("#"+chartId));
+
+      $("#"+chartId).dxRangeSelector({
+                margin: {
+                    top: 5
+                },
+                size: {
+                    height: 100,
+                    width:2000,
+                },
+                scale: {
+                    minorTickInterval: 0.001,
+                    majorTickInterval: 0.005,
+                    label: {
+                        format: "fixedPoint",
+                        precision: 3
+                    }
+                },
+                sliderMarker: {
+                    format: "fixedPoint",
+                    precision: 4,
+                    customizeText: function () {
+                        return this.valueText + " sec";
+                    }
+                },
+                behavior: {
+                    snapToTicks: false
+                },
+                dataSource: [0,180]
+        });
+
+        var btnId="jpId_btn_"+ ix;
+        var btnId="jpId_playat_"+ ix;
+        $("#trackContainer").append( "<button class='btn btn-small btn-warning' id='"+btnId+"'>play</button>");
+        $("#trackContainer").append( "<input class='input-small' id='"+btnId+"' type='text' >");
+
+
+        $("#"+btnId).click( function() {
+          //  console.log('xxx');
+          var offset = $('#'+chartId+'>svg>g.rangeContainer>g.trackers>rect').attr('x');
+          var width = $('#'+chartId+'>svg>g.rangeContainer>g.trackers>rect').attr('width');
+          width-=offset;
+
+          var offsetrange = $('#'+chartId+'>svg>g.rangeContainer>g.trackers>rect').next().attr('x');
+          offsetrange-=offset;
+          var widthrange = $('#'+chartId+'>svg>g.rangeContainer>g.trackers>rect').next().attr('width');
+          widthrange-=offset;
+          
+          console.log(offsetrange);
+          console.log(width);
+
+          
+          var playAhead=(offsetrange/width)*100;
+          // playAhead = parseInt(playAhead,10);
+          console.log(playAhead);
+          $("#"+jplayerId).jPlayer("playHead", playAhead);
+          $("#"+jplayerId).jPlayer("play");
+          console.log(jplayerId);
+        });
+}
+
+          
 $(document).ready(function() {
-  $("#jpId").jPlayer( {
-    ready: function () {
-      $(this).jPlayer("setMedia", {
-        oga: 'http://upload.wikimedia.org/wikipedia/en/b/b5/Radiohead_-_Pyramid_Song_%28sample%29.ogg'
-      })
-    },
-    supplied: "oga",
+  $.ajax({
+       url:"http://172.16.1.175:8888/data/tracks.php?callback=cb",
+       dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+       success:function(json){
+           // do stuff with json (in this case an array)
+           console.log(json);
+           json=json.data;
+           for (var i = 0; i < json.length; i++) {
+             var sound_url = json[i].sound_url;
+             console.log(sound_url);
+             addtrack(i, sound_url);
+           };
+       },
+       error:function(){
+           alert("Error");
+       },
   });
-
-
-
-  $("#omg").click( function() {
-	  //  console.log('xxx');
-  	var offset = $('#chartContainer>svg>g.rangeContainer>g.trackers>rect').attr('x');
-    var width = $('#chartContainer>svg>g.rangeContainer>g.trackers>rect').attr('width');
-    width-=offset;
-
-    var offsetrange = $('#chartContainer>svg>g.rangeContainer>g.trackers>rect').next().attr('x');
-    offsetrange-=offset;
-    var widthrange = $('#chartContainer>svg>g.rangeContainer>g.trackers>rect').next().attr('width');
-    widthrange-=offset;
-    
-    // $("#jpId").jPlayer("play", 0)
-    console.log(offsetrange);
-    console.log(width);
-
-    
-    var playAhead=(offsetrange/width)*100;
-    // playAhead = parseInt(playAhead,10);
-    console.log(playAhead);
-    $("#jpId").jPlayer("playHead", playAhead);
-    $("#jpId").jPlayer("play");
-    console.log('x');
-	});
 });
